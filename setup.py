db17ef6fe6cf735232de10eeb5bd157f156d2a85
Adriana Vasiu <adriana.vasiu@sky.uk>
2017-02-12 22:07:53 +0000

    Test click by adding simple options to the "cdate" -> "calculate date" command

--- setup.py
+++ setup.py
@@ -9,6 +9,6 @@ setup(
     ],
     include_package_data=True,
     entry_points={
-        'console_scripts': ['display-time=timing.conversion:cli']
+        'console_scripts': ['cdate=timing.conversion:cdate']
     }
 )
--- timing/conversion.py
+++ timing/conversion.py
@@ -1,10 +1,11 @@
 import click
-import os
+from datetime import datetime
 
 
 @click.command()
-def cli():
-    print(os.system('time'))
+@click.option('--timestamp', default=1.0, help='Provide a timestamp to be converted')
+def cdate(timestamp):
+    click.echo(datetime.fromtimestamp(float(timestamp)))
 
 if __name__ == '__main__':
-    cli()
+    cdate()
