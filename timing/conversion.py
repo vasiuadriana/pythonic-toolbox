e67ec3ec8919514cf7f6289bc521579701037874
Adriana Vasiu <adriana.vasiu@sky.uk>
2017-02-13 20:46:19 +0000

    Add time to the parameter accepted by the timestamp command

--- timing/conversion.py
+++ timing/conversion.py
@@ -10,10 +10,12 @@ def cdate(timestamp):
 
 
 @click.command()
-@click.option('--date', '-d', type=click.STRING, help='Provide a date in format "Y-m-d" to be converted', default=time.strftime("%c"))
+@click.option('--date', '-d', type=click.STRING,
+              help='Provide a date in format "Y-m-d H:m:s" to be converted',
+              default=time.strftime('%Y-%m-%d %H:%M:%S'))
 def timestamp(date):
     try:
-        date_obj = datetime.strptime(date, '%Y-%m-%d')
+        date_obj = datetime.strptime(date, '%Y-%m-%d %H:%M:%S')
         click.echo(date_obj.strftime("%s"))
     except ValueError:
-        click.echo('Please provide a valid date in the format "Y-m-d"')
+        click.echo('Please provide a valid date in the format "Y-m-d H:M:S"')
